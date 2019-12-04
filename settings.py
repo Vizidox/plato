b8866273da4a27218b46c9ef1fdc4f2118e50feb
Tiago Santos <tiago.santos@vizidox.com>
2019-12-04 15:46:54 +0000

    VDX-501 - Added settings from .env and pyproject.toml

---
+++ settings.py
@@ -0,0 +1,33 @@
+import tomlkit
+from os import environ
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv(), override=False)
+
+
+def _get_project_meta() -> dict:
+    with open(find_dotenv(filename='pyproject.toml'), mode='r') as pyproject:
+        file_contents = pyproject.read()
+
+    return tomlkit.parse(file_contents)['tool']['poetry']
+
+
+__project_meta = _get_project_meta()
+
+PROJECT_NAME = __project_meta["name"]
+PROJECT_VERSION = __project_meta["version"]
+
+POSTGRES_VER = environ["POSTGRES_VER"]
+DB_HOST = environ["DB_HOST"]
+DB_PORT = environ["DB_PORT"]
+DB_USERNAME = environ["DB_USERNAME"]
+DB_PASSWORD = environ["DB_PASSWORD"]
+DB_DATABASE = environ["DB_DATABASE"]
+
+
+def db_url(database_name: str) -> str:
+    return f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{database_name}'
+
+
+WORKING_DB_URL = db_url(DB_DATABASE)
