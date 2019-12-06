f70d19cb26ee5bd24889753868d22e4eebcd4c11
Tiago Santos <tiago.santos@vizidox.com>
2019-12-06 13:03:48 +0000

    VDX-494 - filled in basic methods for template model

--- micro_templating/db/model/models.py
+++ micro_templating/db/model/models.py
@@ -12,3 +12,13 @@ class Template(Base):
     schema = Column(JSONB, nullable=False)
     type = Column(ENUM("text/html"), nullable=False)
     metadata = Column(JSONB, nullable=True)
+
+    def __init__(self, auth_id, id_: str, schema: dict, type_: str, metadata: dict):
+        self.auth_id = auth_id
+        self.id = id_
+        self.schema = schema
+        self.type = type_
+        self.metadata = metadata
+
+    def __repr__(self):
+        return '<Template %r - %r>' % (self.auth_id, self.id)
