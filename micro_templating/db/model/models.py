bd755b8df997e2223bb4534df0402cbd88b4040f
Tiago Santos <tiago.santos@vizidox.com>
2019-12-06 16:17:08 +0000

    VDX-494 - Corrected Template db model

--- micro_templating/db/model/models.py
+++ micro_templating/db/model/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import JSONB, ENUM
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
 
@@ -10,15 +11,15 @@ class Template(Base):
     auth_id = Column(String, primary_key=True)
     id = Column(String, primary_key=True)
     schema = Column(JSONB, nullable=False)
-    type = Column(ENUM("text/html"), nullable=False)
-    metadata = Column(JSONB, nullable=True)
+    type = Column(ENUM("text/html", name="template_mime_type"), nullable=False)
+    metadata_ = Column(JSONB, name="metadata", nullable=True)
 
     def __init__(self, auth_id, id_: str, schema: dict, type_: str, metadata: dict):
         self.auth_id = auth_id
         self.id = id_
         self.schema = schema
         self.type = type_
-        self.metadata = metadata
+        self.metadata_ = metadata
 
     def __repr__(self):
         return '<Template %r - %r>' % (self.auth_id, self.id)
