7a1c8aeb66872b9d863439dfdae5f81d48481161
Tiago Santos <tiago.santos@vizidox.com>
2019-12-12 16:15:21 +0000

    VDX-495 - Added commnet on template file structure

--- micro_templating/flask_app.py
+++ micro_templating/flask_app.py
@@ -19,7 +19,8 @@ swag = Swagger(app)
 
 def load_templates(s3_bucket: str, target_directory: str):
     """
-    Gets templates from the AWS S3 bucket
+    Gets templates from the AWS S3 bucket. Assumes every file inside the bucket is a template.
+    Expected directory structure is /{auth_id}/{template_id}
     Args:
         s3_bucket: AWS S3 Bucket where the templates are
         target_directory: Target directory to store the templates in
