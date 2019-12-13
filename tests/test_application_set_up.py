032ccdd86551f4d8fef70398ff0fc8a7b12e5995
Tiago Santos <tiago.santos@vizidox.com>
2019-12-13 10:28:57 +0000

    VDX-495 added case for updated templates in test

--- tests/test_application_set_up.py
+++ tests/test_application_set_up.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import boto3
 from moto import mock_s3
 from smart_open import s3
@@ -18,12 +19,13 @@ def test_template_loading():
 
     # directory names
     n_of_test_dir = 5
+    encoding = "utf-8"
 
     for i in range(0, n_of_test_dir):
         # file names
         for n in range(0, i+1):
             with s3.open(bucket_name, key_id=f"{i}/{n}", mode="wb") as file:
-                file.write(f"I am file {i}{n}!".encode("latin-1"))
+                file.write(f"I am file {i}{n}!".encode(encoding))
 
     with TemporaryDirectory() as template_dir_name:
         load_templates(bucket_name, template_dir_name)
@@ -37,3 +39,15 @@ def test_template_loading():
 
             assert len(file_list) == int(dir_.name) + 1
 
+        updatee_path = f"{template_dir_name}/{n_of_test_dir - 1}/0"
+        with open(updatee_path, mode='r', encoding=encoding) as updatee_file:
+            assert updatee_file.read() == f"I am file {n_of_test_dir-1}0!"
+
+        # update file
+        with s3.open(bucket_name, key_id=f"{n_of_test_dir - 1}/0", mode="wb") as file:
+            file.write(f"I am an updated file!".encode(encoding))
+
+        load_templates(bucket_name, template_dir_name)
+
+        with open(updatee_path, mode='r', encoding=encoding) as updatee_file:
+            assert updatee_file.read() == f"I am an updated file!"
