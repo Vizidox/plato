e656a18a14b2f6a62d6990bb5ba687cb68a65439
Tiago Santos <tiago.santos@vizidox.com>
2020-01-02 16:50:55 +0000

    VDX-503 - added docstring to auth.py

--- micro_templating/auth.py
+++ micro_templating/auth.py
@@ -10,12 +10,28 @@ oauth_config = requests.get(f"{AUTH_SERVER}/.well-known/openid-configuration").j
 
 
 def token_required(f):
+    """
+    Retrieves most recent JWKs from the OAUTH configuration and sets up the wrapper for JWT validation
+    Args:
+        f: decorated function
 
+    Returns:
+
+    """
     _jwks_json = requests.get(oauth_config['jwks_uri']).json()
     jwks: Dict[str, Dict] = {key_data['kid']: key_data for key_data in _jwks_json['keys']}
 
     @wraps(f)
     def decorated(*args, **kwargs):
+        """
+        Uses the JWKs to validate a JWT token included in the request, adding the account id as the first argument.
+        Args:
+            *args: decorated function's arguments
+            **kwargs: decorated function's keyword arguments
+
+        Returns:
+
+        """
         header = request.headers.get('Authorization', None)
 
         token = header[7:] if header is not None and header.startswith(("Bearer ", "bearer ")) else None
