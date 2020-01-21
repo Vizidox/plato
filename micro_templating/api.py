7712fb537cc26a411bc5ae21725bc05a487eca30
Tiago Santos <tiago.santos@vizidox.com>
2020-01-21 17:14:28 +0000

    VDX-503 - Added template composition endpoint

--- micro_templating/api.py
+++ micro_templating/api.py
@@ -1,8 +1,12 @@
+import tempfile
 from collections import Sequence
 from typing import Optional
 
-from flask import jsonify, request, current_app, g, Flask
+from flask import jsonify, request, current_app, g, Flask, send_file
+from jsonschema import validate as json_validate, ValidationError
+from weasyprint import HTML, CSS
 
+from error_messages import invalid_compose_json, template_not_found
 from .auth import Authenticator
 from .db.models import Template
 from micro_templating.views.views import TemplateDetailView
@@ -31,12 +35,14 @@ def initalize_api(app: Flask, auth: Authenticator, jinjaenv: JinjaEnv):
               $ref: '#/definitions/TemplateDetail'
           404:
             description: Template not found
+        tags:
+           - template
         """
 
         template: Template = Template.query.filter_by(auth_id=g.auth_id).first()
 
         if template is None:
-            return jsonify({"message": f"Template '{template_id}' not found"}), 404
+            return jsonify({"message": template_not_found.format(template_id)}), 404
 
         view = TemplateDetailView(template.id, template.schema, template.type, template.metadata_)
 
@@ -56,6 +62,8 @@ def initalize_api(app: Flask, auth: Authenticator, jinjaenv: JinjaEnv):
             type: array
             items:
                 $ref: '#/definitions/TemplateDetail'
+        tags:
+           - template
         """
 
         all_templates: Sequence[Template] = Template.query.filter_by(auth_id=g.auth_id).all()
@@ -63,3 +71,51 @@ def initalize_api(app: Flask, auth: Authenticator, jinjaenv: JinjaEnv):
                  all_templates]
 
         return jsonify(views)
+
+    @app.route("/template/<string:template_id>/compose", methods=["POST"])
+    def compose_file(template_id: str):
+        """
+        Composes file based on the template
+        ---
+        consumes:
+            - application/json
+        parameters:
+            - in: body
+              name: schema
+              description: body to compose file with, must be according to the template schema
+              schema:
+                type: object
+        security:
+          - api_auth: [templating]
+        responses:
+          201:
+            description: composed file
+            content:
+                application/pdf
+          404:
+             description: Template not found
+        tags:
+           - compose
+           - template
+        """
+        template_model: Template = Template.query.filter_by(auth_id=g.auth_id, template_id=template_id).first()
+
+        if template_model is None:
+            return jsonify({"message": template_not_found.format(template_id)}), 404
+
+        compose_data = request.get_json()
+        try:
+            json_validate(compose_data, template_model.schema)
+        except ValidationError as ve:
+            return jsonify({"message": invalid_compose_json.format(ve.message)}), 400
+
+        template = jinjaenv.get_template(parent=g.auth_id, name=template_id)
+        composed_html = template.render(compose_data)
+
+        with tempfile.NamedTemporaryFile() as target_file:
+            html = HTML(string=composed_html)
+            html.write_pdf(target_file.name)
+            with open(target_file.name, mode='rb') as temp_file_stream:
+                target_file.write(temp_file_stream.read())
+                target_file.seek(0)
+                return send_file(target_file)
