bca101fd841434fdc53d98c087e1833e8a0afea0
Tiago Santos <tiago.santos@vizidox.com>
2020-04-15 16:32:46 +0100

    Add method to set partner_id in tests
    VDX-609

---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+from contextlib import contextmanager
+from flask import appcontext_pushed, g
+
+@contextmanager
+def partner_id_set(app, partner_id):
+    """
+    Sets partner_id in g as a context
+    Args:
+        app:
+        partner_id:
+    """
+    def handler(sender, **kwargs):
+        g.partner_id = partner_id
+    with appcontext_pushed.connected_to(handler, app):
+        yield
