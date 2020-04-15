e06d02916f252cc92ba5182d6c2bc6f748b6aa3e
Tiago Santos <tiago.santos@vizidox.com>
2020-04-15 18:11:56 +0100

    Populate db on templates test
    VDX-609

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,6 +1,29 @@
+import pytest
+
 from tests.conftest import MockAuthenticator
+from micro_templating.db.models import Template
+from micro_templating.db import db
 
 
+@pytest.fixture(scope="class")
+def populate_db(client):
+    with client.application.test_request_context():
+        for i in range(200):
+            t = Template(partner_id="Bob", id_=str(i),
+                         schema={"type": "object",
+                                 "properties": {f"{i}": {"type": "string"}}
+                                 },
+                         type_="text/html", metadata={}, example_composition={}, tags=[])
+            db.session.add(t)
+        db.session.commit()
+
+        yield
+
+        Template.query.delete()
+        db.session.commit()
+
+
+@pytest.mark.usefixtures("populate_db")
 class TestTemplates:
 
     GET_TEMPLATES_ENDPOINT = '/templates/'
