852970df13757733f1d3a6f55ec2bdb49038437f
Tiago Santos <tiago.santos@vizidox.com>
2020-04-24 09:49:47 +0100

    Fix AVAILABLE_IMG_MIME_TYPES constant
    VDX-609

--- micro_templating/compose/__init__.py
+++ micro_templating/compose/__init__.py
@@ -1,3 +1,3 @@
 from .renderer import Renderer, PDF_MIME, OCTET_STREAM, PNG_MIME
 ALL_AVAILABLE_MIME_TYPES = Renderer.renderers.keys()
-AVAILABLE_IMG_MIME_TYPES = [key for key in Renderer.renderers.keys() if "key".startswith("image")]
+AVAILABLE_IMG_MIME_TYPES = [key for key in Renderer.renderers.keys() if key.startswith("image")]
