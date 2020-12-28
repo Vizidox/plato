e1f3aac5c8f9eb2134e740d26db077702ac6958f
Rita Mariquitos <rita.mariquitos@morphotech.co.uk>
2020-12-28 17:09:01 +0000

    Add new error message
    VDX-836

--- plato/error_messages.py
+++ plato/error_messages.py
@@ -10,6 +10,7 @@ token_is_invalid_message = "Token is invalid: {0}"
 
 #templating
 invalid_compose_json = "Invalid compose json: {0}"
+template_already_exists = "Template '{0}' already exists in database"
 template_not_found = "Template '{0}' not found"
 unsupported_mime_type = "No supported format in ACCEPT header: {0}, Available formats: {1}"
 aspect_ratio_compromised = "Specifying both width and height compromises the template's aspect ratio"
